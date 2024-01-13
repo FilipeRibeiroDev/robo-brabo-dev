585328b4cf9b659a7e21e57aac2f73b60c39ccad
Filipe Brito <filipe.brito@hubconexa.com>
2024-01-13 17:59:21 -0300

    Ajuste na API e robo

--- robot/Robot/Driver/BuscarCepDriver.cs
+++ robot/Robot/Driver/BuscarCepDriver.cs
@@ -1,5 +1,6 @@
 ﻿using EasyAutomationFramework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -13,8 +14,48 @@ namespace Robot.Driver
     {
         public BuscarCepDriver() 
         {
-            StartBrowser();
+            this.StartBrowser();
         }
+
+        public EasyReturn.Web StartBrowser(TypeDriver typeDriver = TypeDriver.GoogleChorme, string pathCache = null)
+        {
+            try
+            {
+                switch (typeDriver)
+                {
+                    case TypeDriver.GoogleChorme:
+                        {
+                            var sc = ChromeDriverService.CreateDefaultService();
+                            sc.HideCommandPromptWindow = true;
+                            ChromeOptions c = new ChromeOptions();
+                            c.AddArgument("--start-maximized");
+                            c.AddArgument("--user-data-dir=~/.config/google-chrome");
+                            c.AddArgument("--headless");
+                            c.AddArgument("--no-sandbox");
+                            c.AddArgument("--disable-gpu");
+                            c.AddArgument("--ddisable-dev-shm-usage");
+                            driver = new ChromeDriver(sc, c);
+                            break;
+                        }
+                }
+
+                return new EasyReturn.Web
+                {
+                    driver = driver,
+                    Sucesso = true
+                };
+            }
+            catch (Exception ex)
+            {
+                return new EasyReturn.Web
+                {
+                    driver = driver,
+                    Sucesso = false,
+                    Error = ex.Message.ToString()
+                };
+            }
+        }
+
 
         public void BuscarCep(EnderecoModel endereco)
         {
--- robot/Robot/Program.cs
+++ robot/Robot/Program.cs
@@ -2,18 +2,22 @@
 using Robot.Driver;
 
 
-const string BASE_URL = "https://localhost:7054/Endereco/";
+const string BASE_URL = "http://localhost:5000/Endereco/";
 
 var request = new RequestProvider();
 var buscar = new BuscarCepDriver();
 
 while (true)
-{
+{   
     var endereco = await request.GetAsync<EnderecoModel>(BASE_URL + "ObterCepParaTratamento?robo=robot_dev_1");
 
     if (endereco != null && !string.IsNullOrEmpty(endereco.CEP))
     {
+        Console.WriteLine($"Consultando CEP: {endereco.CEP} para tratamento");
+
         buscar.BuscarCep(endereco);
+
+        Console.WriteLine($"Atualizando dados do CEP: {endereco.CEP} com o bairro {endereco.Bairro}");
 
         await request.PutAsync(BASE_URL + "AtualizarDados", endereco);
     }
